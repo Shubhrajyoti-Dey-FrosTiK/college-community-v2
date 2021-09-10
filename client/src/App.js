17bc8beb011e2ea0a30ee73c33209ca3abf5e99e
shubhrajyotidey <toshubhrajyotidey@gmail.com>
2021-09-10 19:18:52 +0530

    First commit

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h2>Experiment 1 2</h2>
+      <h2>Experiment 1</h2>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
